d9d189507a54c64f1b5c794500ea480f09947565
Sami <acidrisami@outlook.com>
2024-12-04 20:30:09 +0300

    Add files via upload
    First code on my account

---
+++ average.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int test1, test2, average;
+    cout<<"Enter the value of test1 and test2 \n"<<endl;
+    cin>>test1>>test2;
+    average=((test1+test2)/2);
+    cout<<"The average is "<<average<<endl;
+    return 0;
+}
---
+++ do_while.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using std:: cout;
+using std:: cin;
+using std:: endl;
+int main(){
+    int number=0;
+    do{
+        cout<<"Enter a number a number between 1 and 5. \n";
+        cin>>number;
+    }while(number<=1 || number>=5);
+    {
+cout<<"Thanks";
+    }
+}
